75e21b305fb0d541534012b41730199e77d704fb
Yusril <yusril.yusril@bfi.co.id>
2020-06-17 19:26:05 +0700

    add laravel

---
+++ database/migrations/2020_06_16_041138_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('products');
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_produk');
+            $table->double('price');
+            $table->string('merk');
+            $table->integer('stock');
+            $table->bigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
